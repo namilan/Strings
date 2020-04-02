c624aa5d7f952d17173971fb296745c462702104
namilan <nichole.milan@gmail.com>
2020-04-02 16:02:00 -0400

    update

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,25 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// byte ( rare )
+		// short ( rare )
+		// int ( common )
+		// long ( not as often )
+		// float (rare)
+		// double ( common )
+		// char ( not as often )
+		// boolean ( common )
+		
+		String myString = "This is a string";
+		System.out.println("myString is equal to " + myString);
+		myString = myString + ", and this is more.";
+		System.out.println("myString is equal to " + myString);
+		myString = myString + " \u00A9 2019";
+		System.out.println("myString is equal to " + myString);
+		String numberString = "250.55";
+		numberString = numberString + " 49.95";
+		System.out.println(numberString);
+		
 
 	}
 
