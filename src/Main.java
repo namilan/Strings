ee1f9a0b8fc589d5e834c06380ee87dc891a3788
namilan <nichole.milan@gmail.com>
2020-04-02 16:05:07 -0400

    update

--- src/Main.java
+++ src/Main.java
@@ -20,6 +20,13 @@ public class Main {
 		String numberString = "250.55";
 		numberString = numberString + " 49.95";
 		System.out.println(numberString);
+		String lastString = "10";
+		int myInt = 50;
+		lastString = lastString + myInt;
+		System.out.println("LastString is equal to " + lastString);
+		double doubleNumber = 120.47d;
+		lastString = lastString + doubleNumber;
+		System.out.println("LastString is equal to " + lastString);
 		
 
 	}
